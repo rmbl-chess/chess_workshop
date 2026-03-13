fa4e4b909c256b479cf968e24ad39f8dfe95fe9a
hmworsham <hmworsham@gmail.com>
2026-03-13 08:43:18 -0600

    finish setup

--- analysis_scripts/lidar_hack/lidar_startup.R
+++ analysis_scripts/lidar_hack/lidar_startup.R
@@ -1,4 +1,11 @@
+# CHESS Workshop 2026
+# Startup script for CHESS LiDAR data analysis
+# Sets up workspace and loads some critical data
+
+### --- Workspace setup --- ###
+
 # Source config file
+setwd(file.path('~', 'Repos', 'rmbl-chess', 'chess_workshop', 'analysis_scripts', 'lidar_hack'))
 config <- config::get(file=file.path('config.yml'))
 
 # Source helpers and other package functions
@@ -13,13 +20,25 @@ drive_auth(email='hmworsham@lbl.gov')
 
 neon.grid <- st_read(file.path(config$data, 'intermediate', 'neon_2025_lidar_utm_grid.geojson'))
 
-
 # Ingest tree stuff
 trees.demog <- read.csv(ingest.drive(config$extdata$treedemoid)$local_path) # yields a dataframe
-trees.geo <- st_read(ingest.drive(config$extdata$treegeoid)$local_path) # yields an sf object with geometry and attributes
+trees.poly1 <- st_read(ingest.drive(config$extdata$treegeoid1)$local_path) # yields an sf object with geometry and attributes
+trees.poly2 <- st_read(ingest.drive(config$extdata$treegeoid2)$local_path) # yields an sf object with geometry and attributes
 
-# Join geospatial and demographic data
+# Join tree geospatial and demographic data
 trees <- left_join(trees.geo %>% dplyr::select(-Sampling_Area), trees.demog, by='Site_Number')
 
+# LAS catalog
+lascat <- readLAScatalog(file.path(config$extdata$pclocalpath))
+plot(st_geometry(neon.grid))
+plot(x.lascat, col='dodgerblue', add=T)
 
-x.las <- readLAS(ingest.drive(config$extdata$xlasid))
+# CHESS sampling area polygons
+chess.areas <- drive_download(as_id(config$extdata$chessareasid), path=file.path(tempdir(), 'areas.geojson'), overwrite=T)
+chess.areas <- st_read(chess.areas$local_path)
+chess.areas <- st_transform(chess.areas, 'EPSG:32613')
+
+# NEON-delivered DTM and CHM
+neon_chm <- ingest.drive.tif(config$extdata$chmid)
+neon_dtm <- ingest.drive.tif(config$extdata$dtmid)
+
